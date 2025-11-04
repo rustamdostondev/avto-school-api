2e2212864c7c77629443cf15647894364770894f
Dostonbek O'ktamov <rustamdostondev@gmail.com>
2025-11-04 14:32:26 +0500

    refactor: remove unused correct_answer_index column from questions table

---
+++ prisma/migrations/20251104093209_correct_answer_index/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `correct_answer_index` on the `questions` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "questions" DROP COLUMN "correct_answer_index";
